239041be2aab74e02f397538bff2b6edb8aced35
Ian Scott <chesnekov@gmail.com>
2020-06-11 11:45:11 -0700

    add logic to evaluate which roman numeral equivalent the user input is divisible by, reduce number by the relevent amount, and pushes the roman numeral to the output. Does not have logic to account for the 3 of the same numeral in consecuitve positions
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,62 @@
 function romanNumeralConverter (number) {
   let numeralArray = [];
 
-  for (number; number > 0; number--) {
-    numeralArray.push("I");
+  while(number > 0) {
+
+    if (number >= 1000) {
+      const timesDivisibleBy = Math.floor(number / 1000);
+      number = number - (1000 * timesDivisibleBy);
+      for (let i =timesDivisibleBy; i > 0; i--) {
+      numeralArray.push("M");
+      }
+
+    } else if (number >= 500) {
+      const timesDivisibleBy = Math.floor(number / 500);
+      number = number - (500 * timesDivisibleBy);
+      for (let i =timesDivisibleBy; i > 0; i--) {
+      numeralArray.push("D");
+      }
+
+    } else if (number >= 100) {
+      const timesDivisibleBy = Math.floor(number / 100);
+      number = number - (100 * timesDivisibleBy);
+      for (let i =timesDivisibleBy; i > 0; i--) {
+      numeralArray.push("C");
+      }
+
+    } else if (number >= 50) {
+      const timesDivisibleBy = Math.floor(number / 50);
+      number = number - (50 * timesDivisibleBy);
+      for (let i =timesDivisibleBy; i > 0; i--) {
+      numeralArray.push("L");
+      }
+
+    } else if (number >= 10) {
+      const timesDivisibleBy = Math.floor(number / 10);
+      number = number - (10 * timesDivisibleBy);
+      for (let i =timesDivisibleBy; i > 0; i--) {
+      numeralArray.push("X");
+      } 
+
+    } else if (number >=5) {
+      const timesDivisibleBy = Math.floor(number / 5);
+      number = number - (5 * timesDivisibleBy);
+      for (let i =timesDivisibleBy; i > 0; i--) {
+      numeralArray.push("V");
+      } 
+  
+    } else if (number >= 1) {
+      const timesDivisibleBy = Math.floor(number / 1);
+      number = number - timesDivisibleBy;
+      for (let i =timesDivisibleBy; i > 0; i--) {
+      numeralArray.push("I");
+      }
+    }
   }
 
   const numeralString = numeralArray.join("");
-
   return numeralString;
 }
-
 
 
 // UI Logic
@@ -22,6 +69,6 @@ $(document).ready(function(){
 
     const results = romanNumeralConverter (userInput);
 
-    $("#results").text(results)
+    $("#results").text(results);
   });
 });
